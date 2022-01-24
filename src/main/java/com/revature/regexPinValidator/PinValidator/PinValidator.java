fbe971e40cdb63c46c33630b0e99a68762aee268
Andrew Aslakson <andrew01177@gmail.com>
2022-01-24 17:42:58 -0500

    validator now returns false for 5 digit pins

--- src/main/java/com/revature/regexPinValidator/PinValidator/PinValidator.java
+++ src/main/java/com/revature/regexPinValidator/PinValidator/PinValidator.java
@@ -2,6 +2,6 @@ package com.revature.regexPinValidator.PinValidator;
 
 public class PinValidator {
     public static boolean validatePin(String pin) {
-        return pin.matches("\\d\\d\\d\\d?\\d?\\d");
+        return pin.matches("^[0-9]{4}|[0-9]{6}$");
     }
 }
